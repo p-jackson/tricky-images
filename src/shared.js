1b282fa9dd1a390ad9238aa31a7395b07f039d53
Philip Jackson <p-jackson@live.com>
2016-03-18 22:59:15 +1300

    Added redux-thunk implementations of saga 1 and 2

--- src/shared.js
+++ src/shared.js
@@ -2,12 +2,12 @@
 // we've pulled in the libraries using npm.
 const createStore = Redux.createStore;
 const applyMiddleware = Redux.applyMiddleware;
-const takeEvery = ReduxSaga.takeEvery;
-const takeLatest = ReduxSaga.takeLatest;
-const createSagaMiddleware = ReduxSaga.default;
-const isCancelError = ReduxSaga.isCancelError;
-const put = ReduxSaga.effects.put;
-const call = ReduxSaga.effects.call;
+const takeEvery = typeof ReduxSaga !== "undefined" && ReduxSaga.takeEvery;
+const takeLatest = typeof ReduxSaga !== "undefined" && ReduxSaga.takeLatest;
+const createSagaMiddleware = typeof ReduxSaga !== "undefined" && ReduxSaga.default;
+const isCancelError = typeof ReduxSaga !== "undefined" && ReduxSaga.isCancelError;
+const put = typeof ReduxSaga !== "undefined" && ReduxSaga.effects.put;
+const call = typeof ReduxSaga !== "undefined" && ReduxSaga.effects.call;
 
 function readAsDataURL(file) {
    return new Promise((resolve, reject) => {
@@ -39,36 +39,13 @@ function renderAll(props) {
    }
 }
 
-function setupDragging(dispatch) {
+function setupDragging(dispatch, onDropEvent) {
    const image = document.getElementById("image");
 
    image.addEventListener("dragover", ev => ev.preventDefault());
    image.addEventListener("dragenter", () => dispatch({ type: "DRAG_OVER_IMAGE" }));
    image.addEventListener("dragleave", () => dispatch({ type: "DRAG_LEAVE_IMAGE" }));
-
-   image.addEventListener("drop", ev => {
-      ev.preventDefault();
-      dispatch({ type: "DROP_IMAGE", file: getFileFromEvent(ev) })
-   });
-}
-
-// From the MDN example, but returns after finding the first file
-// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
-function getFileFromEvent(ev) {
-   var dt = ev.dataTransfer;
-   if (dt.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      for (var i=0; i < dt.items.length; i++) {
-         if (dt.items[i].kind == "file") {
-            return dt.items[i].getAsFile();
-         }
-      }
-   }
-   else {
-      // Use DataTransfer interface to access the file(s)
-      if (dt.files.length)
-         return dt.files[0];
-   }
+   image.addEventListener("drop", onDropEvent);
 }
 
 const defaultState = {
@@ -87,7 +64,7 @@ function reducer(state, action) {
       case "DROP_IMAGE":
          return Object.assign({}, state, { isImageHovering: false });
       case "START_UPLOADING":
-         return Object.assign({}, state, { isUploading: true });
+         return Object.assign({}, state, { isUploading: true, isImageHovering: false });
       case "STOP_UPLOADING":
          return Object.assign({}, state, { isUploading: false });
       case "CANCEL_UPLOADING":
@@ -99,4 +76,16 @@ function reducer(state, action) {
       default:
          return state;
    }
+}
+
+// This is a copy of the redux-thunk middleware (@2.0.1)
+// It's not available on npmcdn so just pasted it here's since it's tiny
+function thunkMiddleware({ dispatch, getState }) {
+  return next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState);
+    }
+
+    return next(action);
+  };
 }
